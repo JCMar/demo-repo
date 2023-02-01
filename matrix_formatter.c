f6265f8f26373e5d70518f38fe21d54b7e99c50c
markyleangela <markyleangela@gmail.com>
2023-02-01 23:51:02 +0800

    tested the spacer function

--- matrix_formatter.c
+++ matrix_formatter.c
@@ -4,16 +4,28 @@ int spacer(int);
 int matrixDisplay(int*);
 
 int main() {
+    int k;
+    printf("Enter a number: ");
+    scanf("%d", &k);
+    int s = spacer(k);
+    printf("The space occupancy of the entered number: %d[", s);
+    for(int i = 0; i < s; i++) {
+        printf(" ");
+    }
+    printf("]\n");
     return 0;
 }
 
 int spacer(int value)
 {
-    int space=0, x=1, y=10;
-    while(value >= x && value < y) {
+    int space=1, x=10, y=100;
+    while(1) {
+        if((value >= x && value < y) || (value <= -1*(x/10) || value > -1*(y/10) )) {
+            break;
+        }
         x *= 10;
         y *= 10;
-        space++;
+        space += 1;
     }
     return space;
 }
