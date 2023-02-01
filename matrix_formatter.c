0a8ec33a759f658132e7d112340aa851a590e03f
markyleangela <markyleangela@gmail.com>
2023-02-01 23:17:42 +0800

    added the definition of the spacer function

--- matrix_formatter.c
+++ matrix_formatter.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int spacer(int value);
-int matrixDisplay(int* matrix);
+int spacer(int);
+int matrixDisplay(int*);
 
 int main() {
     return 0;
+}
+
+int spacer(int value)
+{
+    int space=0, x=1, y=10;
+    while(value >= x && value < y) {
+        x *= 10;
+        y *= 10;
+        space++;
+    }
+    return space;
 }
